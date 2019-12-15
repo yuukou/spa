14b8733de7836f7253455a9dc691104449e6a01f
田中　優晃 <yuukou.triplejump0219@gmail.com>
2019-12-15 17:16:14 +0900

    タイトル変更と不必要なコンポーネント削除

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import About from '../pages/About'
 import Index from '../pages/Index'
 import Detail from '../pages/Detail'
 
@@ -10,7 +9,6 @@ export default new VueRouter({
   mode: 'history',
   routes: [
     { path: '/', component: Index },
-    { path: '/about', component: About },
     { path: '/detail/:id', component: Detail, name: 'Detail' },
   ],
 })
