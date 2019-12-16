dccdde101646f127333cf418ac4fa4113b9ebe86
田中　優晃 <yuukou.triplejump0219@gmail.com>
2019-12-16 12:00:20 +0900

    bootstrap-vueのインストール

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routes'
 import store from './stores'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
